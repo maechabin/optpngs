4389eb33d2162cc502c54ed18d1b43877c57674c
maeda <takanori@stroly.jp>
2020-10-05 19:21:59 +0900

    first commit

---
+++ optpngs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+[ -z $1 ] && dir="." || dir=${1}
+
+old_pngs_dir=${dir}/old_pngs
+if [ ! -e ${old_pngs_dir} ]; then
+  mkdir ${old_pngs_dir}
+fi
+
+list=`find ${dir} -maxdepth 1 -type f -name '*.png'`
+for file in ${list[@]}; do
+  file_name="${file##*/}"
+  file_size=`wc -c ${file} | awk '{print $1}'`
+  cp ${file} ${old_pngs_dir}/${file_name}
+
+  pngquant --ext .png --force --speed 1 --quality=65-80 ${file}
+
+  optimized_size=`wc -c ${file} | awk '{print $1}'`
+  optimized_rate=`echo "scale=2; $optimized_size / $file_size * 100" | bc`
+
+  echo "${file}  ${file_size} byte ===> ${optimized_size} byte (${optimized_rate})"
+done
---
+++ readme.md
@@ -0,0 +1,21 @@
+## 準備
+
+pngquant をインストール
+
+```
+$ brew install pngquant
+```
+
+## 使い方
+
+カレントディレクトリ内の PNG 画像を最適化
+
+```
+$ ./optpngs.sh
+```
+
+任意のディレクトリを指定して PNG 画像を最適化
+
+```
+$ ./optpngs.sh ./images
+```
